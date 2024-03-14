182a90f63064b07cd419f3cca2727a971892951c
KHUNBIRD <birdpakornpong@gmail.com>
2024-03-14 22:27:19 +0700

    add delay time

--- pages/shop/index.vue
+++ pages/shop/index.vue
@@ -21,7 +21,7 @@
     <div
       class="container"
       data-animation-name="fadeInUpShorter"
-      data-animation-delay="400"
+      data-animation-delay="2000"
       v-animate
     >
       <div class="row">
